858c17a05b4d318414559e2a3dc5975b7b81fb96
Jesus David Estrada De La Hoz <jesusdavide@uninorte.local>
2018-07-27 16:35:57 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Photobook
+## Installation
+Clone the Repository
+
+`git clone https://github.com/jestrade/curso-full-stack-developer.git`
+
+Install dependencies
+
+`npm install`
+
+Run
+
+`npm start`
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <img src={logo} className="App-logo" alt="logo" />
+      </main>
+    );
+  }
+}
+
+export default App;
